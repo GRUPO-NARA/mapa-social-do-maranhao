36fd2b94bc99d3dfd25485422636f1b87725a4be
BrunoAndrade-dev <brunorafha4@gmail.com>
2026-02-25 17:00:35 -0300

    Corrijindo estrutura do projeto e add a pasta elisa-backend

---
+++ elisa-backend/src/main/java/com/example/elisabackend/ElisaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.elisabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElisaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElisaBackendApplication.class, args);
+    }
+
+}
