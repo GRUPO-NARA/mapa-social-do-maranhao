990cd777937cfea8df0c31aac3a8eb3c4b4dfc0a
JulioCesra <juliocesar2006n@hotmail.com>
2026-03-26 21:39:14 -0300

    update: permissão para outra rede

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['192.168.0.31', '10.80.3.123']
+
 };
 
 export default nextConfig;
