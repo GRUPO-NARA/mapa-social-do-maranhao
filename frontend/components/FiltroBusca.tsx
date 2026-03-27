f6879dab7cd8a9b045333e1ee79b297210ca923f
JulioCesra <juliocesar2006n@hotmail.com>
2026-03-26 21:38:43 -0300

    feat: implementação de função responsável pela requisição do nomes dos municipios e atualização automática nos options

--- frontend/components/FiltroBusca.tsx
+++ frontend/components/FiltroBusca.tsx
@@ -1,13 +1,34 @@
-export default function FiltroBusca(){
+"use client"
+import { useEffect, useState } from "react";
+
+
+export default function FiltroBusca() {
+    const [listaMunicipios, setListaMunicipios] = useState([])
+
+    useEffect(() => {
+        getlistaMunicipios()
+    }, [])
+
+    async function getlistaMunicipios() {
+        const response = await fetch("http://localhost:8080/api/nomesMunicipios");
+        const dados = await response.json();
+        setListaMunicipios(dados)
+    }
+
+
     return (
         <div className="justify-center flex p-6 ">
             <div className="w-300 h-fit flex flex-col rounded-2xl shadow-sm bg-white p-6">
                 <h1 className="pb-4"><b>Filtros de Busca</b></h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
                     <div className="flex flex-col gap-1">
-                        <label className="text-[14px]"htmlFor="seletor-municipio">Municipio</label>
+                        <label className="text-[14px]" htmlFor="seletor-municipio">Municipio</label>
                         <select className="rounded-lg shadow-sm p-2" name="seletor-municipio" id="">
-                            <option value="">Fortaleza</option>
+                            {
+                                listaMunicipios.map((nomeMunicipio) => (
+                                    <option key={nomeMunicipio} value={nomeMunicipio}>{nomeMunicipio}</option>
+                                ))
+                            }
                         </select>
                     </div>
                     <div className="flex flex-col gap-1">
