1231fae6d97e722b89a9f2a8d389747197dfaa7b
JulioCesra <juliocesar2006n@hotmail.com>
2026-03-27 16:52:50 -0300

    feat: criação do componente que vai ficar responsável pela visualização do mapa do estado

---
+++ frontend/components/MapaEstado.tsx
@@ -0,0 +1,9 @@
+export default function MapaEstado() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className=" w-300 h-200 shadow-2xl">
+
+            </div>
+        </div>
+    )
+}
