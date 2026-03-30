294b6238a7daa9f3373b767216b189b130482e4a
JulioCesra <juliocesar2006n@hotmail.com>
2026-03-30 17:57:30 -0300

    feat: criação de novo componente responsável pelas informações gerais do município (População Total, PIB per capita e etc...)

---
+++ frontend/components/ResumoGeral.tsx
@@ -0,0 +1,32 @@
+export default function ResumoGeral(){
+    return (
+        <div className="flex justify-center items-center">
+            <div className="flex flex-col gap-5 w-300 p-6 rounded-2xl shadow-xl/30 shadow-sky-600 border border-sky-600">
+                <div className="flex items-center gap-2">
+                    <p className="w-1 h-6 rounded bg-sky-600"></p>
+                    <h1 className="text-xl font-bold">Resumo Geral</h1>
+                </div>
+                <div className="grid grid-cols-3 gap-4 ">
+                    <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                        <div className="flex flex-col gap-2 p-6">
+                            <h1 className="text-gray-600">População Total</h1>
+                            <h1 className="text-sky-600 font-bold text-2xl">12.500.000</h1>
+                        </div>
+                    </div>
+              <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                        <div className="flex flex-col gap-2 p-6">
+                            <h1 className="text-gray-600">PIB per capita</h1>
+                            <h1 className="text-sky-600 font-bold text-2xl">R$ 80.000</h1>
+                        </div>
+                    </div>
+                    <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                        <div className="flex flex-col gap-2 p-6">
+                            <h1 className="text-gray-600">Razão de Sexo</h1>
+                            <h1 className="text-sky-600 font-bold text-2xl">98.3</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
