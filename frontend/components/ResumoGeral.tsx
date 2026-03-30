8afd2a2e8851d852003ed37371c11fc6de364842
Júlio César Costa da Silva <109476501+JulioCesra@users.noreply.github.com>
2026-03-30 20:49:20 -0300

    update: adição de animações ao passar o mouse.

--- frontend/components/ResumoGeral.tsx
+++ frontend/components/ResumoGeral.tsx
@@ -7,19 +7,19 @@ export default function ResumoGeral(){
                     <h1 className="text-xl font-bold">Resumo Geral</h1>
                 </div>
                 <div className="grid grid-cols-3 gap-4 ">
-                    <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                    <div className="bg-white rounded-2xl border-sky-600 border transition-all duration-300 hover:-translate-y-1">
                         <div className="flex flex-col gap-2 p-6">
                             <h1 className="text-gray-600">População Total</h1>
                             <h1 className="text-sky-600 font-bold text-2xl">12.500.000</h1>
                         </div>
                     </div>
-              <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                    <div className="bg-white rounded-2xl border-sky-600 border transition-all duration-300 hover:-translate-y-1">
                         <div className="flex flex-col gap-2 p-6">
                             <h1 className="text-gray-600">PIB per capita</h1>
                             <h1 className="text-sky-600 font-bold text-2xl">R$ 80.000</h1>
                         </div>
                     </div>
-                    <div className="bg-white h-30 rounded-2xl border-sky-600 border">
+                    <div className="bg-white rounded-2xl border-sky-600 border transition-all duration-300 hover:-translate-y-1">
                         <div className="flex flex-col gap-2 p-6">
                             <h1 className="text-gray-600">Razão de Sexo</h1>
                             <h1 className="text-sky-600 font-bold text-2xl">98.3</h1>
