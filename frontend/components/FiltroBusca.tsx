2e388a5d330dd1b35133202d247b5d1765a56902
Carlos Amorim <carlosrodriguesreal@gmail.com>
2026-04-03 02:20:17 -0300

    update: adição de novas funcionalidades do filtro de busca e do mapa.
    Ajusta o componente de filtro e de mapa, além da criação de um novo componente de Painel onde armazena toda a parte de "use client", deixando o page.tsx apenas para server

--- frontend/components/FiltroBusca.tsx
+++ frontend/components/FiltroBusca.tsx
@@ -1,59 +1,84 @@
 "use client"
-import { useEffect, useState } from "react";
 
+import { useEffect, useState } from "react"
 
 interface FiltroBuscaProps {
-    nomeSelecionado: string;
-    aoMudarMunicipio: (municipio: string) => void;
+  nomeSelecionado: string
+  aoMudarMunicipio: (municipio: string) => void
 }
 
-export default function FiltroBusca({ nomeSelecionado, aoMudarMunicipio }: FiltroBuscaProps) {
-    const [listaMunicipios, setListaMunicipios] = useState<string[]>([])
-
-    useEffect(() => {
-        getlistaMunicipios()
-    }, [])
-
-    async function getlistaMunicipios() {
-        const response = await fetch("http://localhost:8080/api/nomesMunicipios");
-        const dados = await response.json();
-        setListaMunicipios(dados)
+type GeoJSONData = {
+    features: {
+    properties: {
+    name: string
     }
+  }[]
+}
 
+export default function FiltroBusca({ nomeSelecionado, aoMudarMunicipio,}: FiltroBuscaProps) {
+  const [listaMunicipios, setListaMunicipios] = useState<string[]>([])
 
-    return (
-        <div className="justify-center flex p-6 ">
-            <div className="w-300 h-fit flex flex-col gap-4 rounded-2xl shadow-xl/30 shadow-sky-600 p-6 border-1 border-sky-600">
-                <div className="flex items-center gap-2 ">
-                    <p className="w-1 h-6 rounded bg-sky-600"></p>
-                    <h1 className="text-xl font-bold">Filtros de Busca</h1>
-                </div>
-        
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-                    <div className="flex flex-col gap-1">
-                        <label className="text-sm" htmlFor="seletor-municipio">Municipio</label>
-                        <select
-                            className="rounded-lg shadow-sm p-2 bg-sky-600 text-white"
-                            name="seletor-municipio"
-                            value={nomeSelecionado}
-                            onChange={(e) => aoMudarMunicipio(e.target.value)}
-                        >
-                            <option value="">Selecione um município</option>
-                            {
-                                listaMunicipios.map((municipio) => (
-                                    <option key={municipio} value={municipio}>{municipio}</option>
-                                ))
-                            }
-                        </select>
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label className="text-sm" htmlFor="seletor-ano">Ano</label>
-                        <select className="rounded-lg shadow-sm p-2 bg-sky-600 text-white" name="seletor-ano" id="">
-                            <option value="">2024</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-        </div>
+  useEffect(() => { getListaMunicipios() }, [])
+
+  async function getListaMunicipios() {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/tbrugz/geodata-br/master/geojson/geojs-21-mun.json"
     )
+
+    const dados: GeoJSONData = await response.json()
+
+    const nomesMunicipios = dados.features.map(
+      (feature) => feature.properties.name
+    ) 
+
+    nomesMunicipios.sort((a, b) => a.localeCompare(b, "pt-BR"))
+
+    setListaMunicipios(nomesMunicipios)
+  }
+
+  return (
+    <div className="justify-center flex p-6">
+      <div className="w-full max-w-[1200px] h-fit flex flex-col gap-4 rounded-2xl shadow-xl/30 shadow-sky-600 p-6 border border-sky-600">
+        <div className="flex items-center gap-2">
+          <p className="w-1 h-6 rounded bg-sky-600"></p>
+          <h1 className="text-xl font-bold">Filtros de Busca</h1>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-1">
+            <label className="text-sm" htmlFor="seletor-municipio">
+              Município
+            </label>
+
+            <select
+              id="seletor-municipio"
+              className="rounded-lg shadow-sm p-2 bg-sky-600 text-white"
+              value={nomeSelecionado}
+              onChange={(e) => aoMudarMunicipio(e.target.value)}
+            >
+              <option value="">Selecione um município</option>
+
+              {listaMunicipios.map((municipio) => (
+                <option key={municipio} value={municipio}>
+                  {municipio}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label className="text-sm" htmlFor="seletor-ano">
+              Ano
+            </label>
+            <select
+              id="seletor-ano"
+              className="rounded-lg shadow-sm p-2 bg-sky-600 text-white"
+            >
+              <option value="">2024</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- frontend/components/Indicadores.tsx
+++ frontend/components/Indicadores.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react";
 import IndicadoresPrincipais from "./IndicadoresPrincipais";
 import IndicadoresSociais from "./IndicadoresSociais";
