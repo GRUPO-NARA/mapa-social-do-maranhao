5952ae253951bafb31d51afc10aad8afb8c693b0
Júlio César Costa da Silva <109476501+JulioCesra@users.noreply.github.com>
2026-04-08 23:26:35 -0300

    feat: adição de requisição para área territorial.
    Foi criado o endpoint (GET) dos dados da área territorial por município. Também foi criado a requisição no frontend e atualização automática no componente do resumo geral.

--- backend/src/main/java/com/mapasocialma/backend/controller/dadosGeograficosController.java
+++ backend/src/main/java/com/mapasocialma/backend/controller/dadosGeograficosController.java
@@ -1,5 +1,6 @@
 package com.mapasocialma.backend.controller;
 
+import com.mapasocialma.backend.repository.AreaTerritorialRepository;
 import com.mapasocialma.backend.repository.DensidadeDemograficaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,19 @@ public class dadosGeograficosController {
     @Autowired
     private DensidadeDemograficaRepository densidadeDemograficaRepository;
 
+    @Autowired
+    private AreaTerritorialRepository areaTerritorialRepository;
+
     @GetMapping("/densidade_demografica")
     public ResponseEntity<List<Double>> getDensidadeDemografica(@RequestParam String nomeMunicipio){
         List<Double> densidade_demografica = densidadeDemograficaRepository.findDensidadeDemograficaByMunicipio(nomeMunicipio);
         return ResponseEntity.ok(densidade_demografica);
     }
+
+    @GetMapping("/area_territorial")
+    public ResponseEntity<List<Double>> getAreaTerritorial(@RequestParam String nomeMunicipio){
+        List<Double> area_territorial = areaTerritorialRepository.findAreaTerritorialByMunicipio(nomeMunicipio);
+        return ResponseEntity.ok(area_territorial);
+    }
+
 }
