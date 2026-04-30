e1a3a32caff1b57a6ec77d912d95d00c0a7f94bd
JulioCesra <juliocesar2006n@hotmail.com>
2026-04-30 10:07:48 -0300

    update: adição de NATIVE QUERY para consulta com os parâmetros de ano e município

--- src/backend/src/main/java/mpma/mapasocial/backend/repository/demograficos/IndiceDeDesenvolvimentoHumanoRepository.java
+++ src/backend/src/main/java/mpma/mapasocial/backend/repository/demograficos/IndiceDeDesenvolvimentoHumanoRepository.java
@@ -2,10 +2,14 @@ package mpma.mapasocial.backend.repository.demograficos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import mpma.mapasocial.backend.entity.demograficos.IndiceDeDesenvolvimentoHumanoEntity;
+import mpma.mapasocial.backend.entity.demograficos.indiceDeDesenvolvimentoHumanoEntity;
 
 @Repository
-public interface IndiceDeDesenvolvimentoHumanoRepository extends JpaRepository<IndiceDeDesenvolvimentoHumanoEntity, Long> {
-
+public interface IndiceDeDesenvolvimentoHumanoRepository extends JpaRepository<indiceDeDesenvolvimentoHumanoEntity, Long> {
+    @Query(value = "SELECT idh.valor FROM demograficos.indice_de_desenvolvimento_humano idh\n" +
+            "JOIN dados_estadual.referencias_codigos_municipais i ON idh.cod_municipio = i.codigo_ibge\n" +
+            "WHERE idh.referencia = :ano AND i.municipio ILIKE :municipio;", nativeQuery = true)
+    Double buscarIndiceDeDesenvolvimentoHumanoPorMunicipio(@Param("ano") Integer ano, @Param("municipio") String municipio);
 }
